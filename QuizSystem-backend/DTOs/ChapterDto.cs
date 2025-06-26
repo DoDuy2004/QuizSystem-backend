337545174aa173add1932a988b0f2dd87ed63523
DoDuy2004 <135083040+DoDuy2004@users.noreply.github.com>
2025-06-26 12:12:01 +0700

    Fixes

--- QuizSystem-backend/DTOs/ChapterDto.cs
+++ QuizSystem-backend/DTOs/ChapterDto.cs
@@ -1,7 +1,16 @@
-﻿namespace QuizSystem_backend.DTOs
+﻿using QuizSystem_backend.Models;
+
+namespace QuizSystem_backend.DTOs
 {
     public class ChapterDto
     {
+        public ChapterDto() { }
+        public ChapterDto(Chapter chapter) 
+        { 
+            Id = chapter.Id;
+            Name = chapter.Name;
+            Subject = chapter.Course.Subject;
+        }
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
 

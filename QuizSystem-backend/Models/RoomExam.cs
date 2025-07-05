b17f82dcb27dbaa8e9480090e4d126f5b72671c3
Shin <hoanganhle1697@gmail.com>
2025-07-06 02:06:44 +0700

    fixx

--- QuizSystem-backend/Models/RoomExam.cs
+++ QuizSystem-backend/Models/RoomExam.cs
@@ -12,7 +12,7 @@ namespace QuizSystem_backend.Models
         public Status Status { get; set; }
         public Guid CourseClassId { get; set; }
         public Subject SubjectId { get; set; } = null!; // Assuming SubjectId is a Guid, adjust as necessary
-       
+        
 
         // Navigation   
         public virtual CourseClass Course { get; set; } = null!;
